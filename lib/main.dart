a8f1f72137870d838bf4e1faff855d82c20e09ba
sayedanbia1 <sayedanbia5555@gamil.com>
2023-06-16 10:51:57 +0430

    start projec

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: container();
+      )),
+    ),
+  ));
+}
